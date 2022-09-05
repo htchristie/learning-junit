4ee8576a68d334d5cd943a2b1c61ada715d780b7
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-05 16:58:45 -0300

    testing all methods from class Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    @Test
+    @DisplayName("Add two numbers") // specifies a more convenient and informative name for the test.
+    void add() {
+        // check if our method correctly adds 2 and 2
+        assertEquals(4, Calculator.add(2, 2));
+    }
+
+    @Test
+    @DisplayName("Multiply two numbers")
+    void multiply() {
+        // multiple assertions in a single test and execute all of them regardless of whether some of them fail
+        /* The assertAll() method takes a series of assertions in form of lambda expressions and ensures all of them are
+         checked */
+
+        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+                () -> assertEquals(-4, Calculator.multiply(2, -2)),
+                () -> assertEquals(4, Calculator.multiply(-2, -2)),
+                () -> assertEquals(0, Calculator.multiply(1, 0)));
+    }
 }
